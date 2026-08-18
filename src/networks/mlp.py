5217faa482a93c2e2082ab92b61b06beb69729e1
asd <macintoshkrug@gmail.com>
2026-08-18 21:42:35 +0300

    improve reinforce

--- src/networks/mlp.py
+++ src/networks/mlp.py
@@ -2,10 +2,12 @@ from torch import nn
 
 
 class MLP(nn.Module):
-    def __init__(self, n_observations, n_actions, hidden=128):
+    def __init__(self, n_observations, n_actions, hidden=64):
         super().__init__()
         self.net = nn.Sequential(
             nn.Linear(n_observations, hidden),
+            nn.LeakyReLU(),
+            nn.Linear(hidden, hidden),
             nn.LeakyReLU(),
             nn.Linear(hidden, n_actions),
             nn.Softmax(dim=-1),
