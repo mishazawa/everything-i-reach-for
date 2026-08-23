fe7bc8741e757425340aefab1b6c951f4d980812
asd <macintoshkrug@gmail.com>
2026-08-23 22:26:10 +0300

    nothing helps

--- Makefile
+++ Makefile
@@ -1,7 +1,6 @@
 .PHONY: lab run tensorboard
 
 LOG_DIR := "./logs"
-RELOAD_INTERVAL := 5
 
 lab:
 	uv run jupyter lab --notebook-dir=.
@@ -10,4 +9,4 @@ run:
 	uv run main.py
 
 tensorboard:
-	uv run tensorboard --logdir $(LOG_DIR) --reload_interval $(RELOAD_INTERVAL)
+	uv run tensorboard --logdir $(LOG_DIR)
--- src/agents/cart_ppo.py
+++ src/agents/cart_ppo.py
@@ -4,6 +4,7 @@ from src.agents.cart_a2c import A2CCartAgent
 from src.agents.common import calc_advantage
 
 EPS = 0.2
+ENTROPY_COEFF = 0.01
 
 
 class PPOCartAgent(A2CCartAgent):
@@ -12,17 +13,21 @@ class PPOCartAgent(A2CCartAgent):
 
         # crutch to make it compatible with a2c api.
         # actual ratio calculated outside of method.
-        ratio: torch.Tensor = probs  # pyright: ignore[reportAssignmentType]
+        ratio: torch.Tensor = probs[0]  # pyright: ignore[reportAssignmentType]
+        entropy = probs[1]
 
         surr1 = ratio * advantage
         surr2 = torch.clamp(ratio, 1 - EPS, 1 + EPS) * advantage
-        loss = -torch.min(surr1, surr2).mean()
+        actor_loss = -torch.min(surr1, surr2).mean()
+        loss = actor_loss - entropy * ENTROPY_COEFF
 
         self.optimizer_policy.zero_grad()
         loss.backward()
         self.optimizer_policy.step()
 
-    def evaluate_action(self, obs: torch.Tensor, actions: torch.Tensor) -> torch.Tensor:
+    def evaluate_action(
+        self, obs: torch.Tensor, actions: torch.Tensor
+    ) -> tuple[torch.Tensor, torch.Tensor]:
         probs = self.policy.forward(obs)
         dist = torch.distributions.Categorical(probs)
-        return dist.log_prob(actions)
+        return dist.log_prob(actions), dist.entropy().mean()
